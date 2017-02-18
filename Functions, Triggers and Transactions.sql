ba6856ecfd93d34c36b6e64bf06b08c8d13234e4
Marin Gerin <marin_gerin@abv.bg>
2017-02-18 02:10:38 +0200

    MORE STUFF

--- Functions, Triggers and Transactions.sql
+++ Functions, Triggers and Transactions.sql
@@ -1,10 +1,51 @@
--- Problem 1.	Employees with Salary Above 35000
+USE SoftUni
+GO
 
+-- Problem 1.	Employees with Salary Above 35000
+GO
+CREATE PROC usp_GetEmployeesSalaryAbove35000
+AS
+BEGIN
+	SELECT FirstName, LastName FROM Employees
+	WHERE Salary > 35000
+END
+
+EXEC dbo.usp_GetEmployeesSalaryAbove35000
+	
 -- Problem 2.	Employees with Salary Above Number
+GO
+CREATE PROC usp_GetEmployeesSalaryAboveNumber(@Number MONEY)
+AS
+BEGIN
+	SELECT FirstName, LastName FROM Employees
+	WHERE Salary >= @Number
+END
+
+EXEC dbo.usp_GetEmployeesSalaryAboveNumber '35000'
 
 -- Problem 3.	Town Names Starting With
 
+GO
+CREATE PROC usp_GetTownsStartingWith(@pattern VARCHAR(10))
+AS
+BEGIN
+	SELECT [Name] FROM Towns
+	WHERE SUBSTRING(Name, 1, LEN(@pattern)) = @pattern
+END
+
+EXEC dbo.usp_GetTownsStartingWith 'B'
+
+DROP PROC dbo.usp_GetTownsStartingWith
+
 -- Problem 4.	Employees from Town
+
+GO
+CREATE PROC usp_GetEmployeesFromTown(@townName VARCHAR(50))
+AS
+BEGIN
+	SELECT e.FirstName, e.LastName FROM Employees AS e
+	WHERE (SELECT FROM TO)
+END
 
 -- Problem 5.	Salary Level Function
 
@@ -26,7 +67,18 @@ END;
 SELECT e.FirstName, E.LastName, dbo.ufn_GetSalaryLevel(e.Salary) AS SalaryLevel FROM Employees AS e;
 
 -- Problem 6.	Employees by Salary Level
+GO
 
+CREATE PROC usp_EmployeesBySalaryLevel(@salaryLevel VARCHAR(10))
+AS
+BEGIN
+	SELECT FirstName, LastName FROM Employees
+	WHERE dbo.ufn_GetSalaryLevel(Salary) = @salaryLevel
+END
+
+GO
+
+EXEC dbo.usp_EmployeesBySalaryLevel 'High'
 -- Problem 7.	Define Function
 
 -- Problem 8.	* Delete Employees and Departments
@@ -89,3 +141,43 @@ BEGIN
 
 	COMMIT
 END;
+
+-- Problem 16.	Money Transfer
+
+-- Problem 17.	Create Table Logs
+
+-- Problem 18.	Create Table Emails
+
+-- Problem 19.	*Scalar Function: Cash in User Games Odd Rows
+
+-- Problem 20.	Trigger
+
+-- Problem 21.	*Massive Shopping
+
+-- Problem 22.	Number of Users for Email Provider
+
+-- Problem 23.	All User in Games
+
+-- Problem 24.	Users in Games with Their Items
+
+-- Problem 25.	* User in Games with Their Statistics
+
+-- Problem 26.	All Items with Greater than Average Statistics
+
+-- Problem 27.	Display All Items with Information about Forbidden Game Type
+
+-- Problem 28.	Buy Items for User in Game
+
+-- Problem 29.	Peaks and Mountains
+
+-- Problem 30.	Peaks with Their Mountain, Country and Continent
+
+-- Problem 31.	Rivers by Country
+
+-- Problem 32.	Count of Countries by Currency
+
+-- Problem 33.	Population and Area by Continent
+
+-- Problem 34.	Monasteries by Country
+
+-- Problem 35.	Monasteries by Continents and Countries
