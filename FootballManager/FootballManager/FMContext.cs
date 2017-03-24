931190016e4250a8c151e210a01115c718bc290e
Marin Gerin <marin_gerin@abv.bg>
2017-03-25 00:20:33 +0200

    new things

---
+++ FootballManager/FootballManager/FMContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.AccessControl;
+
+namespace FootballManager
+{
+    using System.Data.Entity;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Models;
+
+    public class FMContext : DbContext
+    {
+        public FMContext() : base("FootballManagerContext")
+        {
+            
+        }
+
+        private DbSet<Team> Teams { get; set; }
+
+        private DbSet<Player> Players { get; set; }
+        private DbSet<Manager> Managers { get; set; }
+        private DbSet<League> Leagues { get; set; }
+    }
+}
--- FootballManager/FootballManager/Models/Team.cs
+++ FootballManager/FootballManager/Models/Team.cs
@@ -8,8 +8,14 @@ namespace FootballManager.Models
 {
     public class Team
     {
+        public Team()
+        {
+            this.Players = new HashSet<Player>();
+            this.Leagues = new HashSet<League>();
+        }
         public int TeamId { get; set; }
         public string Name { get; set; }
         public ICollection<Player> Players { get; set; }
+        public ICollection<League> Leagues { get; set; }
     }
 }
