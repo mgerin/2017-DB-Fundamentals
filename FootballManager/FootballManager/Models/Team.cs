a8997b00a849a6553c60d4b5d21fb172b8cdbe7f
mgerin <marin_gerin@abv.bg>
2017-03-24 21:28:36 +0200

    CodeFirst

---
+++ FootballManager/FootballManager/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManager.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Player> Players { get; set; }
+    }
+}
---
+++ FootballManager/FootballManager/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace FootballManager
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
